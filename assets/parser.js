47103249979518a77002dafc5c8a311bd98b485e
Thundo <thundo@protonmail.com>
2023-02-27 22:49:31 +0100

    Add menu functionality

--- assets/parser.js
+++ assets/parser.js
@@ -2,7 +2,7 @@ import { capitalizeFirstLetter, splitRoute } from './utils.js';
 
 export function parseTimetable(station, isDeparture = true) {
     let trainSet = [], train = {}, stopList = [];
-    let data = window.dataAsJson;
+    let data = window.timetablesAsJson;
     if (data === null) { return; }
     for (let i in data) {
         if (data[i]['timetable'] === undefined) { continue; }
@@ -52,15 +52,123 @@ export function parseTimetable(station, isDeparture = true) {
     return trainSet.sort((a, b) => { return a.timestamp - b.timestamp });
 }
 
+function generateStationsList() {
+    let stationsData = window.stationDataAsJson;
+    let stationsSet = [], station = {};
+    stationsData.forEach((stationData) => {
+        if (stationData['availability'] === 'abandoned') { return; }
+
+        station['name'] = stationData['name'];
+        station['isActive'] = false;
+
+        if (stationData['checkpoints'] === null || stationData['checkpoints'] === '') {
+            station['nameApi'] = station['name'];
+            station['checkpoints'] = station['name'];
+        } else {
+            station['nameApi'] = stationData['checkpoints'].split(';')[0];
+            station['checkpoints'] = stationData['checkpoints'];
+        }
+
+        stationsSet.push(station);
+        station = {};
+    });
+
+    window.stationsSet = stationsSet.sort((a, b) => { return a.name.localeCompare(b.name) });
+    refreshSceneriesList();
+}
+
+export function refreshSceneriesList() {
+    let activeSceneries = $('#active-sceneries');
+    let otherSceneries = $('#other-sceneries');
+    let sceneries = $('#sceneries');
+    let selectedOption = sceneries.val();
+    activeSceneries.empty();
+    otherSceneries.empty();
+
+    window.stationsSet.forEach((station) => {
+        station['isActive'] = false;
+        
+        window.activeStationsAsJson.forEach((activeStation) => {
+            if (activeStation['region'] !== "eu") { return; }
+            if (!activeStation['isOnline']) { return; }
+            if (activeStation['stationName'] === station['name']) {
+                station['isActive'] = true;
+            }
+        });
+
+        if (station['isActive']) {
+            activeSceneries.append($('<option>', {
+                text: station['name'],
+            }));
+        } else {
+            otherSceneries.append($('<option>', {
+                text: station['name']
+            }));
+        }
+    });
+
+    sceneries.val(selectedOption);
+}
+
+export function refreshCheckpointsList() {
+    let checkpoints = $('#checkpoints');
+    checkpoints.empty();
+
+    window.stationsSet.forEach((station) => {
+        if (station['name'] === $('#sceneries').val()) {
+            station['checkpoints'].split(';').forEach((checkpoint) => {
+                if (checkpoint === station['nameApi']) {
+                    checkpoints.append($('<option>', {
+                        value: checkpoint.replace('LCS ', ''),
+                        text: station['name'].replace('LCS ', '')
+                    }));
+                } else {
+                    checkpoints.append($('<option>', {
+                        value: checkpoint.replace('LCS ', ''),
+                        text: checkpoint.replace('LCS ', '')
+                    }));
+                }
+            });
+        }
+    });
+}
+
 export function getTimetables() {
     $.ajax({
         url: 'https://spythere.pl/api/getActiveTrainList',
         dataType: 'json',
         //url: 'https://gist.githubusercontent.com/Thundo54/bba89c9eba39921844eec0013c9c1c40/raw/96b70b491f68d7400b5ac11fce8d54f226f4047c/gistfile1.txt',
         //url: 'https://gist.githubusercontent.com/Thundo54/8f66268e1b36bdf92b40f26e50f652dc/raw/090f14935868e814ff071363b6b90ca0fb7849f6/gistfile1.txt',
-        success: (response) => {
-            window.dataAsJson = response;
-        }
+        success: [
+            (response) => {
+                window.timetablesAsJson = response;
+            }
+        ]
+    });
+}
+
+export function getStationsData() {
+    $.ajax({
+        url: 'https://spythere.pl/api/getSceneries',
+        dataType: 'json',
+        success: [
+            (response) => {
+                window.stationDataAsJson = response;
+                generateStationsList();
+            }
+        ]
+    });
+}
+
+export function getActiveStations() {
+    $.ajax({
+        url: 'https://api.td2.info.pl/?method=getStationsOnline',
+        dataType: 'json',
+        success: [
+            (response) => {
+                window.activeStationsAsJson = response['message'];
+            }
+        ]
     });
 }
 
