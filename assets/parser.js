32a8cfc30644d1522290e7cf891cf91cac43c1c3
Thundo <thundo@protonmail.com>
2023-03-04 19:22:54 +0100

    Rewrite timetables parser

--- assets/parser.js
+++ assets/parser.js
@@ -1,52 +1,101 @@
 import { capitalizeFirstLetter, splitRoute } from './utils.js';
 
-export function parseTimetable(station, isDeparture = true) {
-    let trainSet = [], train = {}, stopList = [];
-    let data = window.timetablesAsJson;
-    if (data === null) { return; }
-    for (let i in data) {
-        if (data[i]['timetable'] === undefined) { continue; }
-        for (let j in data[i]['timetable']['stopList']) {
-            if (data[i]['timetable']['stopList'][j]['stopType'].includes('ph')) {
-                stopList.push(capitalizeFirstLetter(data[i]['timetable']['stopList'][j]['stopNameRAW'].split(',')[0]));
-            }
-            if (capitalizeFirstLetter(data[i]['timetable']['stopList'][j]['stopNameRAW']) === capitalizeFirstLetter(station)) {
-                if (!data[i]['timetable']['stopList'][j]['terminatesHere']
-                    && !data[i]['timetable']['stopList'][j]['beginsHere']) {
-                    if (!data[i]['timetable']['stopList'][j]['stopType'].includes('ph')) {
-                        continue;
-                    }
-                }
-                if (data[i]['timetable']['stopList'][j]['confirmed']) { continue; }
-                if (isDeparture && !data[i]['timetable']['stopList'][j]['terminatesHere']) {
-                    train['beginsTerminatesHere'] = data[i]['timetable']['stopList'][j]['beginsHere'];
-                    train['timestamp'] = data[i]['timetable']['stopList'][j]['departureTimestamp'];
-                    train['delay'] = data[i]['timetable']['stopList'][j]['departureDelay'];
-                    train['stationFromTo'] = splitRoute(data[i]['timetable']['route'])[1];
-                } else if (!isDeparture && !data[i]['timetable']['stopList'][j]['beginsHere']) {
-                    train['beginsTerminatesHere'] = data[i]['timetable']['stopList'][j]['terminatesHere'];
-                    train['timestamp'] = data[i]['timetable']['stopList'][j]['arrivalTimestamp'];
-                    train['delay'] = data[i]['timetable']['stopList'][j]['arrivalDelay'];
-                    train['stationFromTo'] = splitRoute(data[i]['timetable']['route'])[0];
-                }
-                train['stoppedHere'] = data[i]['timetable']['stopList'][j]['stopped'];
-            }
-        }
+// export function parseTimetable(station, isDeparture = true) {
+//     let trainSet = [], train = {}, stopList = [];
+//     let data = window.timetablesAsJson;
+//     if (data === null) { return; }
+//     for (let i in data) {
+//         if (data[i]['timetable'] === undefined) { continue; }
+//         for (let j in data[i]['timetable']['stopList']) {
+//             if (data[i]['timetable']['stopList'][j]['stopType'].includes('ph')) {
+//                 stopList.push(capitalizeFirstLetter(data[i]['timetable']['stopList'][j]['stopNameRAW'].split(',')[0]));
+//             }
+//             if (capitalizeFirstLetter(data[i]['timetable']['stopList'][j]['stopNameRAW']) === capitalizeFirstLetter(station)) {
+//                 if (!data[i]['timetable']['stopList'][j]['terminatesHere']
+//                     && !data[i]['timetable']['stopList'][j]['beginsHere']) {
+//                     if (!data[i]['timetable']['stopList'][j]['stopType'].includes('ph')) {
+//                         continue;
+//                     }
+//                 }
+//                 if (data[i]['timetable']['stopList'][j]['confirmed']) { continue; }
+//                 if (isDeparture && !data[i]['timetable']['stopList'][j]['terminatesHere']) {
+//                     train['beginsTerminatesHere'] = data[i]['timetable']['stopList'][j]['beginsHere'];
+//                     train['timestamp'] = data[i]['timetable']['stopList'][j]['departureTimestamp'];
+//                     train['delay'] = data[i]['timetable']['stopList'][j]['departureDelay'];
+//                     train['stationFromTo'] = splitRoute(data[i]['timetable']['route'])[1];
+//                 } else if (!isDeparture && !data[i]['timetable']['stopList'][j]['beginsHere']) {
+//                     train['beginsTerminatesHere'] = data[i]['timetable']['stopList'][j]['terminatesHere'];
+//                     train['timestamp'] = data[i]['timetable']['stopList'][j]['arrivalTimestamp'];
+//                     train['delay'] = data[i]['timetable']['stopList'][j]['arrivalDelay'];
+//                     train['stationFromTo'] = splitRoute(data[i]['timetable']['route'])[0];
+//                 }
+//                 train['stoppedHere'] = data[i]['timetable']['stopList'][j]['stopped'];
+//             }
+//         }
+//
+//         if (!data[i]['timetable']['category'].match(/^[LTZ]/) && train['timestamp'] !== undefined) {
+//             if (isDeparture) {
+//                 train['timetable'] = stopList.slice(stopList.indexOf(station) + 1);
+//             } else {
+//                 train['timetable'] = stopList.slice(0, stopList.indexOf(station));
+//             }
+//             train['category'] = data[i]['timetable']['category'];
+//             train['trainNo'] = data[i]['trainNo'];
+//             trainSet.push(train);
+//             //console.log(train);
+//         }
+//         stopList = [];
+//         train = {};
+//     }
+//
+//     window.trainsSetBefore = trainSet.sort((a, b) => { return a.timestamp - b.timestamp });
+//     return trainSet.sort((a, b) => { return a.timestamp - b.timestamp });
+// }
 
-        if (!data[i]['timetable']['category'].match(/^[LTZ]/) && train['timestamp'] !== undefined) {
+
+export function parseTimetable() {
+    let trainSet = [], train = {}, stopList = [];
+    if (window.timetablesAsJson === null) { return; }
+    window.timetablesAsJson.forEach((timetable) => {
+        if (timetable['timetable'] === undefined) { return; }
+        timetable['timetable']['stopList'].forEach((stopPoint) => {
+            if (stopPoint['stopType'].includes('ph')) {
+                stopList.push(capitalizeFirstLetter(stopPoint['stopNameRAW'].split(',')[0]));
+            }
+            if (capitalizeFirstLetter(stopPoint['stopNameRAW']) === capitalizeFirstLetter(station)) {
+                if (!stopPoint['terminatesHere'] && !stopPoint['beginsHere']) {
+                     if (!stopPoint['stopType'].includes('ph')) { return; }
+                }
+                if (stopPoint['confirmed']) { return; }
+                train['stoppedHere'] = stopPoint['stopped'];
+                if (isDeparture && !stopPoint['terminatesHere']) {
+                    train['beginsTerminatesHere'] = stopPoint['beginsHere'];
+                    train['timestamp'] = stopPoint['departureTimestamp'];
+                    train['delay'] = stopPoint['departureDelay'];
+                    train['stationFromTo'] = splitRoute(timetable['timetable']['route'])[1];
+                } else if (!isDeparture && !stopPoint['beginsHere']) {
+                    train['beginsTerminatesHere'] = stopPoint['terminatesHere'];
+                    train['timestamp'] = stopPoint['arrivalTimestamp'];
+                    train['delay'] = stopPoint['arrivalDelay'];
+                    train['stationFromTo'] = splitRoute(timetable['timetable']['route'])[0];
+                }
+            }
+        });
+
+        if (timetable['timetable']['category'].match(/^[LTZ]/) && train['timestamp'] !== undefined) {
             if (isDeparture) {
                 train['timetable'] = stopList.slice(stopList.indexOf(station) + 1);
             } else {
                 train['timetable'] = stopList.slice(0, stopList.indexOf(station));
             }
-            train['category'] = data[i]['timetable']['category'];
-            train['trainNo'] = data[i]['trainNo'];
+            train['category'] = timetable['timetable']['category'];
+            train['trainNo'] = timetable['trainNo'];
             trainSet.push(train);
-            //console.log(train);
         }
+
         stopList = [];
         train = {};
-    }
+    });
 
     window.trainsSetBefore = trainSet.sort((a, b) => { return a.timestamp - b.timestamp });
     return trainSet.sort((a, b) => { return a.timestamp - b.timestamp });
